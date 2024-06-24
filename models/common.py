64da091d80f2030ecb912a5ef6ed77b4b6d69d9f
magic524 <1026060507@qq.com>
2024-06-24 15:21:11 +0800

    s

--- models/common.py
+++ models/common.py
@@ -715,36 +715,6 @@ def autopad(k, p=None, d=1):
         p = (k - 1) // 2 * d if isinstance(k, int) else [(x - 1) // 2 * d for x in k]
     return p
 
-
-############################################
-
-        # Prepare spatial bias
-        b_conv = torch.zeros(conv.weight.size(0)) if conv.bias is None else conv.bias
-        b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
-        fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
-
-    return fusedconv
-
-class RepNCSPELAN5(nn.Module):
-    def __init__(self, c1, c2, c3, c4, c5=1):  
-        super().__init__()
-        self.c = c3//2
-        self.cv1 = Conv(c1, c3, 1, 1)
-        self.cv2 = nn.Sequential(RepNCSP(c3//2, c4, c5), Conv(c4, c4, 3, 1))
-        self.cv3 = nn.Sequential(RepNCSP(c4, c4, c5), Conv(c4, c4, 3, 1))
-        self.cv4 = Conv(c3+(2*c4), c2, 1, 1)
-
-    def forward(self, x):
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend((m(y[-1])) for m in [self.cv2, self.cv3])
-        return self.cv4(torch.cat(y, 1))
-    
-    def fuse(self):
-        self.cv1 = fuse_conv_and_bn(self.cv1[0], self.cv1[1])
-        self.cv2[1] = fuse_conv_and_bn(self.cv2[1][0], self.cv2[1][1])
-        self.cv3[1] = fuse_conv_and_bn(self.cv3[1][0], self.cv3[1][1])
-        self.cv4 = fuse_conv_and_bn(self.cv4[0], self.cv4[1])
-
 ##################################################################################
 ##### YOLOR #####
 
